6f699c89670af02677c0cc8a6399097a0f0a740d
Tiago Ssa <tiago.silva301@gmail.com>
2021-10-31 00:11:44 +0100

    Navbar styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './assets/styles/custom.scss';
 import './App.css';
+import NavBar from 'components/Navbar/navbar';
 
-function App() {
+const App = () => {
   return (
-
+    <>
+    <NavBar/>
     <h1>TopCars</h1>
-    
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Navbar/navbar.tsx
@@ -0,0 +1,37 @@
+import './styles.css';
+import 'bootstrap/js/src/collapse.js';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
+      <div className="container-fluid">
+        <a href="link nav-logo-text">
+          <h4>TopCars</h4>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#topcars-navbar"
+          aria-controls="topcars-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="topcars-navbar">
+          <ul className="navbar-nav offset-md-2 main-menu">
+            <li>
+              <a href="link" className="active">HOME</a>
+            </li>
+            <li>
+              <a href="link">CATALOG</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
